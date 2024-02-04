23d9c6da5a0d5fee8dfa4a9d3b790301c2a51a23
dannyreiser <dannyreiser@gmail.com>
2024-02-04 15:37:31 +0200

    - added column headers

---
+++ src/components/SpreadsheetHeaderRow.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {SpreadsheetCell} from "./SpreadsheetCell";
+import {isNumberObject} from "util/types";
+
+export function SpreadsheetHeaderRow(props: {
+    numberOfColumns: number
+}) {
+
+    const columnData = Array.from(new Array(props.numberOfColumns + 1)).map((v: any, idx: number) => {
+        return idx === props.numberOfColumns ? 'SUM' : String.fromCharCode('A'.charCodeAt(0) + idx);
+    });
+
+    return (
+        <div className={'row header-row'}>
+            {columnData.map((value: string, idx: number) => (
+                <div className={'cell'}>{value}</div>
+            ))}
+        </div>
+    )
+}
--- src/components/SpreadsheetView.tsx
+++ src/components/SpreadsheetView.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {SpreadsheetRow} from "./SpreadsheetRow";
+import {SpreadsheetHeaderRow} from "./SpreadsheetHeaderRow";
 
 export function SpreadsheetView(props: {
     name: string,
@@ -13,10 +14,16 @@ export function SpreadsheetView(props: {
         props.updateData(newData);
     }
 
+    const numberOfColumns = props.data[0] ? props.data[0].length : 0;
+
     return (
         <div className={'spreadsheet-container'}>
             <div className={'header'}>{props.name}</div>
+            <div className={'header-row'}>
+
+            </div>
             <div className={'rows'}>
+                <SpreadsheetHeaderRow numberOfColumns={numberOfColumns} />
                 {props.data.map((rowData: number[], idx: number) => (
                     <SpreadsheetRow key={idx}
                                     rowIndex={idx}
